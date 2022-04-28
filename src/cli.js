3833f3421eb22e6646ecaa3f860701bf1c98e352
Alexander Poole <alex.o.poole@gmail.com>
2022-04-28 05:53:20 +0200

    Add interaction with lens proxy contract

--- src/cli.js
+++ src/cli.js
@@ -1,89 +1,43 @@
 #!/usr/bin/env node
-doc = `
-Usage:
-  ethscan (abi | code) <address> [options]
-  ethscan -h | --help | --version
-
-Options:
- -h --help              Show this screen.
- -v, --version          Show version.
- -o, --output=<file>    Write to output file.
- -d, --debug            Print debug logs.
-
-Example:
-  $ ethscan abi 0xD33526068D116cE69F19A9ee46F0bd304F21A51f
-  $ ethscan code 0xD33526068D116cE69F19A9ee46F0bd304F21A51f -o ./code
-`;
-const { docopt } = require("docopt");
-const { API } = require("./etherscan");
-const fs = require("fs");
-const { version } = require("../package.json");
-const { saver } = require("./saver");
 require("dotenv").config();
-
-const arguments = docopt(doc, {
-  version: version,
-});
-
-const apiKey = process.env.ETHSCAN_KEY;
-const apiUrl = process.env.ETHSCAN_URL;
+const {lens} = require("./lensContracts")
+const {ethers, Wallet} = require("ethers");
 
 function exitError(error) {
-  console.error(error);
-  process.exit(1);
+    console.error(error);
+    process.exit(1);
 }
 
-const log = (message) => {
-  if (arguments["--debug"]) console.warn(message);
-};
+const privateKey = process.env.PRIVATE_KEY;
+const rpcUrl = process.env.RPC_URL;
 
-if (arguments["--version"]) {
-  console.log(version);
-  process.exit(0);
+console.log(rpcUrl);
+
+async function main() {
+    const provider = new ethers.providers.WebSocketProvider(rpcUrl)
+    console.log(await provider.getBlockNumber())
+    const signer = new Wallet(privateKey, provider);
+    console.log(signer.address);
+
+    const mockProfile = new ethers.Contract(lens.MockProfileCreationProxy.address, lens.MockProfileCreationProxy.abi, signer);
+
+    const address = signer.address;
+    const MOCK_URI = 'https://ipfs.io/ipfs/QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR';
+    const MOCK_FOLLOW_NFT_URI =
+        'https://ipfs.fleek.co/ipfs/ghostplantghostplantghostplantghostplantghostplantghostplan'
+
+
+        // For some reason followModuleInitData is undefined when it comes to encoding it in ethers.
+        // Follow the contract creation code and we should be able to find out why that is.
+    const txHash = await mockProfile.proxyCreateProfile({
+        to: address,
+        handle: "owodunni",
+        imageURI: MOCK_URI,
+        followModule: "0x0000000000000000000000000000000000000000",
+        followModuleInitData: [],
+        followNFTURI: MOCK_FOLLOW_NFT_URI,
+    });
+    console.log(txHash);
 }
 
-log(arguments);
-
-if (!apiKey) {
-  log(`
-  No API key found for etherscan. This will reduce the amout of requests
-  that can be made. Make sure to set the environment variable 'ETHERSCAN=API_KEY'
-
-  Read more about etherscan api keys here:
-  https://docs.etherscan.io/
-  `);
-}
-
-const address = arguments["<address>"];
-
-if (!address) {
-  exitError("No address provided!");
-}
-
-const output = arguments["--output"];
-
-const api = API(apiKey, apiUrl);
-let action = null;
-if (arguments.abi) {
-  action = api.getAbi(address).then((r) => {
-    const result = JSON.stringify(r, null, 2);
-    if (output) {
-      fs.writeFileSync(output, result);
-    } else {
-      console.log(result);
-    }
-  });
-} else if (arguments.code) {
-  action = api.getCode(address).then((r) => {
-    const sources = saver.format(r.sources);
-    if (output) {
-      return saver.save(output, sources);
-    } else {
-      sources.forEach(console.log);
-    }
-  });
-} else {
-  exitError(`No valid actions found in ${JSON.stringify(arguments)}`);
-}
-
-action.then(() => process.exit(0)).catch(exitError);
+main().then(() => process.exit(0)).catch(exitError);
---
+++ src/lensContracts.js
@@ -0,0 +1,6 @@
+exports.lens = {
+    MockProfileCreationProxy: {
+        address: "0x39c9Bc23B1F993B94dEC69B7Ac11C95145EC4e15",
+        abi: [{"inputs":[{"internalType":"address","name":"hub","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"components":[{"internalType":"address","name":"to","type":"address"},{"internalType":"string","name":"handle","type":"string"},{"internalType":"string","name":"imageURI","type":"string"},{"internalType":"address","name":"followModule","type":"address"},{"internalType":"bytes","name":"followModuleData","type":"bytes"},{"internalType":"string","name":"followNFTURI","type":"string"}],"internalType":"struct DataTypes.CreateProfileData","name":"vars","type":"tuple"}],"name":"proxyCreateProfile","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+    }
+}
